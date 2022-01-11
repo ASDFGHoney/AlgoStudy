3e941f9016387c1b62275d8444e48878aa343195
Chi-Honey <necktop9711@gmail.com>
2022-01-11 10:02:27 +0900

    samsung sw academy

---
+++ SWE_no1/solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class orderr{
+public:
+    int mNum;
+    int mQuant;
+    int mPri;
+
+} od;
+
+class marcket{
+public:
+    int curNum;
+} mart;
+
+void init()
+{
+}
+
+int buy(int mNumber, int mStock, int mQuantity, int mPrice)
+{
+
+}
+
+
+int sell(int mNumber, int mStock, int mQuantity, int mPrice)
+{
+    return 0;
+}
+
+void cancel(int mNumber)
+{
+}
+
+int bestProfit(int mStock)
+{
+    return 0;
+}
